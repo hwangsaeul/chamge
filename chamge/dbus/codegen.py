7a437be7f8a07874c5cab0bb0dc4f7d59eee652e
Jakub Adam <jakub.adam@collabora.com>
2019-10-03 13:58:15 +0200

    codegen.py: fix compatibility with Ubuntu 16.04
    Old gdbus-codegen doesn't recognize --output-directory parameter.
    Passing full output file path without '.c' or '.h' to --generate-c-code
    instead should work with all versions.

--- chamge/dbus/codegen.py
+++ chamge/dbus/codegen.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import os
 import subprocess
 import sys
 
@@ -8,9 +9,8 @@ name = 'org.hwangsaeul.Chamge1.' + sys.argv[1]
 subprocess.call([
   'gdbus-codegen',
   '--interface-prefix=' + name + '.',
-  '--generate-c-code=' + sys.argv[2],
+  '--generate-c-code=' + os.path.join(sys.argv[3], sys.argv[2]),
   '--c-namespace=ChamgeDBus',
   '--annotate', name, 'org.gtk.GDBus.C.Name', sys.argv[1],
-  '--output-directory=' + sys.argv[3],
   sys.argv[4]
 ])
